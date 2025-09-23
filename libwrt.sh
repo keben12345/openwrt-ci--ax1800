85eaf7857dcb4f52e59adcc08cffd1bea2a38bfb
keben12345 <79047578+keben12345@users.noreply.github.com>
2025-09-24 06:44:10 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -3,3 +3,4 @@ git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone --depth=1 https://github.com/vernesong/OpenClash.git package/openclash
+git clone --depth=1 https://github.com/LiBwrt/nss-packages.git package/nss-packages
